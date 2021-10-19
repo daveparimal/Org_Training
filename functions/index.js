3ff2fbd4d5ab25febd4b2638befae27efba4bd15
devAsifAli <asif.ali@napses.com>
2021-10-19 15:14:24 +0530

    remove unwanted code for running the repo

--- functions/index.js
+++ functions/index.js
@@ -17,7 +17,7 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-require("./passport")(app);
+// require("./passport")(app);
 require('./api-routes');
 
 app.use((req, res, next) => {
